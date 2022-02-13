16cbf65b30edd19304eda10def5ec7babe3a940c
Izzy Lancaster <avrisaac555@gmail.com>
2022-02-12 22:09:11 -0800

    Switched to using flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A log file analyzer written in rust.";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
+    fenix.url = github:nix-community/fenix;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = {self, nixpkgs, fenix, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      rust-package = fenix.packages.${system};
+    in
+      {
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          pname = "fox";
+          version = "0.0.1";
+          src = ./.;
+          buildInputs = [
+            rust-package.default.toolchain
+            rust-package.rust-analyzer
+          ];
+        };
+      }
+    );
+}
